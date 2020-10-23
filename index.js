56b414f45d4f3099ef64837cbbeee60f3968c9e7
Garrett Breach <breachg1992@gmail.com>
2020-10-23 18:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,8 @@ function deleteFromObjectByKey(recipes, key) {
   delete newRecipes.key
   return newRecipes
 }
+
+function destructivelyDeleteFromObjectByKey (recipes, key) {
+  delete recipes[key];
+  return recipes
+}
